08d28fac1dd3a87be89bcf1116459a32016571f0
ThomasHoutekier <thomas.houtekier@reaktika.be>
2021-07-15 14:02:38 +0200

    WIP: implement the aggregation entity to keep track of minimum and maximum temperatures

--- src/main/java/be/reaktika/weatherstation/domain/Aggregations.java
+++ src/main/java/be/reaktika/weatherstation/domain/Aggregations.java
@@ -10,6 +10,9 @@ import com.google.protobuf.Empty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 @ValueEntity(entityType = "aggregations")
 public class Aggregations {
 
@@ -25,12 +28,57 @@ public class Aggregations {
     @CommandHandler
     public Reply<Empty> registerTemperature(RecordTemperatureCommand command, CommandContext<WeatherStationAggregations.Aggregations> ctx) {
         logger.info("registering Temperature " + command);
+        var sorted = command.getMeasurementsList()
+                .stream()
+                .sorted(Comparator.comparingDouble(TemperatureMeasurement::getMeasuredTemperature))
+                .collect(Collectors.toList());
+        TemperatureMeasurement highestInEvent = sorted.get(sorted.size()-1);
+        TemperatureMeasurement lowestInEvent = sorted.get(0);
+
+        WeatherStationExtremes currentExtremes = ctx.getState().map(WeatherStationAggregations.Aggregations::getExtremes).orElse(WeatherStationExtremes.getDefaultInstance());
+        WeatherStationExtremes.Builder newExtremesBuilder = WeatherStationExtremes.newBuilder(currentExtremes);
+
+        TemperatureRecord previousMaxRecord = currentExtremes.hasMaxTemperature() ? currentExtremes.getMaxTemperature() : TemperatureRecord.newBuilder().setCurrent(highestInEvent).build();
+        TemperatureRecord previousMinRecord = currentExtremes.hasMinTemperature() ? currentExtremes.getMinTemperature() : TemperatureRecord.newBuilder().setCurrent(lowestInEvent).build();
+
+        //initialze the state for the first event
+        if (!currentExtremes.hasMaxTemperature()) {
+            newExtremesBuilder.setMaxTemperature(previousMaxRecord);
+        }
+        if (!currentExtremes.hasMinTemperature()){
+            newExtremesBuilder.setMinTemperature(previousMinRecord);
+        }
+
+        WeatherStationAggregations.Aggregations.Builder stateBuilder;
+        if (ctx.getState().isPresent()){
+            stateBuilder = WeatherStationAggregations.Aggregations.newBuilder(ctx.getState().get());
+        }else {
+            stateBuilder = WeatherStationAggregations.Aggregations.newBuilder()
+                    .setExtremes(WeatherStationExtremes.newBuilder().setMaxTemperature(TemperatureRecord.newBuilder().setCurrent(highestInEvent))
+                                                                     .setMinTemperature(TemperatureRecord.newBuilder().setCurrent(lowestInEvent)));
+        }
+        if(highestInEvent.getMeasuredTemperature() > previousMaxRecord.getCurrent().getMeasuredTemperature()){
+            logger.info("high temperature record is broken");
+            newExtremesBuilder.setMaxTemperature(TemperatureRecord.newBuilder()
+                    .setCurrent(highestInEvent)
+                    .setPreviousRecord(previousMinRecord.getCurrent()));
+        }
+        if(lowestInEvent.getMeasuredTemperature() < previousMinRecord.getCurrent().getMeasuredTemperature()){
+            logger.info("low temperature record is bnroken");
+            newExtremesBuilder.setMinTemperature(TemperatureRecord.newBuilder()
+                    .setCurrent(lowestInEvent)
+                    .setPreviousRecord(previousMinRecord.getCurrent()));
+        }
+        var newExtremes = newExtremesBuilder.build();
+        var newState = stateBuilder.setExtremes(newExtremes).build();
+        ctx.updateState(newState);
+
         return Reply.message(Empty.getDefaultInstance());
     }
 
 
     @CommandHandler
-    public Reply<Empty> RegisterWindspeed(RecordWindspeedCommand command, CommandContext<WeatherStationAggregations.Aggregations> ctx) {
+    public Reply<Empty> registerWindspeed(RecordWindspeedCommand command, CommandContext<WeatherStationAggregations.Aggregations> ctx) {
         logger.info("registering Windspeed " + command);
         return Reply.message(Empty.getDefaultInstance());
     }
