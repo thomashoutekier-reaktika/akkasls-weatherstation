b72c95d5e4b7054be4c9e9dad388fbd7b06dc4a0
ThomasHoutekier <thomas.houtekier@reaktika.be>
2021-07-15 14:56:26 +0200

    introduce a view for the aggregations

--- src/main/java/be/reaktika/weatherstation/Main.java
+++ src/main/java/be/reaktika/weatherstation/Main.java
@@ -6,6 +6,10 @@ import be.reaktika.weatherstation.domain.Aggregations;
 import be.reaktika.weatherstation.domain.WeatherStationAggregations;
 import be.reaktika.weatherstation.domain.WeatherStationDomain;
 import be.reaktika.weatherstation.domain.WeatherStationEntity;
+import be.reaktika.weatherstation.view.WeatherStationAggregationsView;
+import be.reaktika.weatherstation.view.WeatherStationAverageViewImpl;
+import be.reaktika.weatherstation.view.WeatherstationAverageView;
+import be.reaktika.weatherstation.view.WeatherstationExtremesView;
 import com.akkaserverless.javasdk.AkkaServerless;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +27,16 @@ public final class Main {
             .registerAction(WeatherStationPublishApiImpl.class,
                     WeatherStationApi.getDescriptor().findServiceByName("WeatherStationApiService"),
                     WeatherStationApi.getDescriptor()
-                    );
+                    )
+            .registerView(WeatherStationAverageViewImpl.class,
+                    WeatherstationAverageView.getDescriptor().findServiceByName("WeatherStationOverallAverage"),
+                    "weatherstationOverallAverage",
+                    WeatherstationAverageView.getDescriptor())
+            .registerView(
+                    WeatherStationAggregationsView.class,
+                    WeatherstationExtremesView.getDescriptor().findServiceByName("WeatherStationExtremes"),
+                    "aggregations",
+                    WeatherstationExtremesView.getDescriptor());
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
 
---
+++ src/main/java/be/reaktika/weatherstation/view/WeatherStationAggregationsView.java
@@ -0,0 +1,21 @@
+package be.reaktika.weatherstation.view;
+
+import be.reaktika.weatherstation.domain.WeatherStationAggregations.*;
+import com.akkaserverless.javasdk.view.UpdateHandler;
+import com.akkaserverless.javasdk.view.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+@View
+public class WeatherStationAggregationsView {
+
+    private final static Logger logger = LoggerFactory.getLogger(WeatherStationAggregationsView.class);
+
+    @UpdateHandler
+    public Aggregations updateAggregations(Aggregations update, Optional<Aggregations> state){
+        logger.info("updating aggregationview ");
+        return update;
+    }
+}
